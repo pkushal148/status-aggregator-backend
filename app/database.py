200dbb44925c00145a6fc603f52e2ddcde16ac82
pkushal148 <pkushal148@gmail.com>
2025-12-23 18:26:59 +0530

    db connection retry

--- app/database.py
+++ app/database.py
@@ -7,11 +7,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
-    connect_args={
-        "sslmode": "require"
-    }
+     pool_size=5,
+    max_overflow=10
 )
-SessionLocal = sessionmaker(bind=engine)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
