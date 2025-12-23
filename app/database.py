2f3c942c06f69a162e2c28649da7dc407aec5777
pkushal148 <pkushal148@gmail.com>
2025-12-23 18:03:31 +0530

    enable ssl for supabase

--- app/database.py
+++ app/database.py
@@ -4,7 +4,13 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={
+        "sslmode": "require"
+    }
+)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
